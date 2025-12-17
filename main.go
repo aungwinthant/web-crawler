29bec3577a5f3338b98fd8277d8ba7aafe1e54da
root <root@DESKTOP-N5VH9BM.>
2025-12-17 23:00:49 +0700

    added : mongodb and chores

--- main.go
+++ main.go
@@ -2,27 +2,58 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"hash/fnv"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"strings"
 	"sync"
 	"time"
 
+	URL "net/url"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"golang.org/x/net/html"
 )
 
+var pages = make([]WebPage, 10)
+
 type WebPage struct {
-	Title   string
-	URL     string
-	Content string
+	Title     string
+	URL       string
+	Content   string
+	CrawledAt int64
 }
 type Queue struct {
 	total    int
 	mu       sync.Mutex
 	messages []string
+}
+
+type CrawlData struct {
+	data map[uint64]bool
+	mu   sync.Mutex
+}
+
+func (c *CrawlData) Add(url string) {
+	c.mu.Lock()
+
+	defer c.mu.Unlock()
+
+	c.data[getHash(url)] = true
+}
+
+func (c *CrawlData) Size() int {
+	c.mu.Lock()
+
+	defer c.mu.Unlock()
+
+	return len(c.data)
 }
 
 func (q *Queue) Enqueue(url string) {
@@ -48,15 +79,81 @@ func (q *Queue) Dequeue() (string, bool) {
 
 	return url, true
 }
+func getHash(url string) uint64 {
+	bytes, err := json.Marshal(url)
 
+	if err != nil {
+		return 0
+	}
+
+	h := fnv.New64a()
+	h.Write(bytes)
+	return h.Sum64()
+}
+func (c *CrawlData) IsVisited(url string) bool {
+
+	hash := getHash(url)
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, found := c.data[hash]
+	return found
+}
+
+type DB struct {
+	Client     *mongo.Client
+	Collection *mongo.Collection
+}
+
+func InitDB(uri string) *DB {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	opts := options.Client().ApplyURI(uri)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		panic(fmt.Errorf("could not ping mongodb: %w", err))
+	}
+	fmt.Println("Successfully connected to MongoDB")
+	return &DB{
+		Client:     client,
+		Collection: client.Database("Crawler").Collection("WebPage"),
+	}
+
+}
+
+func (DB *DB) Insert(webpage WebPage) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	DB.Collection.InsertOne(ctx, webpage)
+}
 func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading env file!")
+	}
+
+	var DB *DB
+
+	mongoURI := os.Getenv("MONGO_HOST")
+
+	if len(mongoURI) > 0 {
+		DB = InitDB(mongoURI)
+	}
 
 	args := os.Args[1:]
 
 	var wg sync.WaitGroup
 
 	if len(args) == 0 {
-		fmt.Println("go run main.go seedurl")
+		fmt.Println("go run main.go seedurl.")
 	}
 
 	url := args[0]
@@ -68,49 +165,60 @@ func main() {
 		messages: make([]string, 0),
 	}
 
+	crawled := CrawlData{
+		data: make(map[uint64]bool, 1024),
+	}
 	queue.Enqueue(url)
 
-	StartWorker(&queue, 2)
-	// for i := 1; i <= 5000; i++ {
-	// 	wg.Add(1)
-	// 	defer wg.Done()
+	crawled.data[getHash(url)] = true
 
-	// 	go StartWorker(&myQueue, i)
-	// }
+	// StartWorker(&queue, 2, &crawled)
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go StartWorker(&queue, i, &crawled, &wg, DB)
+	}
 
 	wg.Wait()
+
+	fmt.Printf("Crawled : %v records!\n", crawled.Size())
+
+	for _, page := range pages {
+		fmt.Printf("%v : \t%v\n %v\n\n", page.URL, page.Title, page.Content)
+	}
 }
 
-func fetchPage(url string) []byte {
+func fetchPage(url string, worker int) []byte {
 
-	fmt.Printf("fetching url from %v\n", url)
+	fmt.Printf("worker #%v fetching url from %v\n", worker, url)
 
 	resp, err := http.Get(url)
 
 	if err != nil {
 		fmt.Printf("Error fetching url : %v\n", err)
+		return []byte{}
 	}
 	defer resp.Body.Close()
 
 	result, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Printf("Error reading response body : %v\n", err)
+		return []byte{}
 	}
 	return result
 }
-func WriteToFile(webPage WebPage) {
-	err := os.WriteFile(os.TempDir(), []byte(fmt.Sprintf("%v : %v\n", webPage.URL, webPage.Content)), 0644)
 
-	if err != nil {
-		log.Fatalf("Cannot open file %v\n", err)
-	}
-}
-func parseHtml(content []byte, queue *Queue) {
+func parseHtml(url string, content []byte, queue *Queue, crawled *CrawlData, DB *DB) {
 	z := html.NewTokenizer(bytes.NewReader(content))
 
-	webPage := WebPage{Title: "", URL: "", Content: ""}
+	webPage := WebPage{Title: "", URL: url, Content: ""}
 
+	baseurl, _ := URL.Parse(url)
+
+	var body, title bool
+
+	var bodyContent bytes.Buffer
 	for {
+
 		if z.Next() == html.ErrorToken {
 			if z.Err() == io.EOF {
 				break // End of the document
@@ -121,10 +229,21 @@ func parseHtml(content []byte, queue *Queue) {
 
 		tt := z.Token()
 
-		if tt.Type == html.StartTagToken {
+		switch tt.Type {
+		case html.StartTagToken:
 			if tt.Data == "javascript" || tt.Data == "script" || tt.Data == "style" {
 				// Skip script and style tags
 				z.Next()
+				continue
+			}
+
+			if tt.Data == "title" {
+				title = true
+				continue
+			}
+
+			if tt.Data == "p" {
+				body = true
 				continue
 			}
 			if tt.Data == "a" {
@@ -132,32 +251,65 @@ func parseHtml(content []byte, queue *Queue) {
 				for _, attr := range tt.Attr {
 					if attr.Key == "href" {
 
-						if len(attr.Val) > 0 && strings.HasPrefix(attr.Val, "https://") {
-							webPage.URL = attr.Val
-							queue.Enqueue(webPage.URL)
+						url = attr.Val
+
+						if len(url) > 0 && (strings.HasPrefix(url, "https://") || strings.HasPrefix(url, "/")) {
+
+							if strings.HasPrefix(url, "/") {
+								url = baseurl.Host + url
+							}
+
+							visited := crawled.IsVisited(url)
+
+							if visited {
+								continue
+							} else {
+								crawled.Add(url)
+								queue.Enqueue(url)
+							}
 						}
 					}
 				}
-				if z.Next() == html.TextToken {
-					webPage.Title = z.Token().Data
-					fmt.Printf("Found url : %v : %v\n", webPage.Title, webPage.URL)
-				}
+			}
+		case html.TextToken:
+
+			if title {
+				webPage.Title = strings.TrimSpace(tt.Data)
+				title = false
+			}
+			if body {
+				bodyContent.WriteString(strings.TrimSpace(tt.Data))
+			}
+		case html.EndTagToken:
+			if tt.Data == "p" {
+				body = false
+				webPage.Content = strings.TrimSpace(bodyContent.String())
+
 			}
 		}
 	}
+
+	webPage.CrawledAt = time.Now().UnixMilli()
+	DB.Insert(webPage)
 }
 
-func StartWorker(queue *Queue, workerID int) {
+func StartWorker(queue *Queue, workerID int, crawled *CrawlData, wg *sync.WaitGroup, DB *DB) {
 	fmt.Printf("starting worker : %v\n", workerID)
-	for {
+
+	for crawled.Size() < 500 {
 		url, success := queue.Dequeue()
 
 		if success {
-			result := fetchPage(url)
+			result := fetchPage(url, workerID)
 
-			parseHtml(result, queue)
+			if len(result) > 0 {
+				parseHtml(url, result, queue, crawled, DB)
+			}
 		} else {
 			time.Sleep(100 * time.Millisecond)
 		}
 	}
+	defer wg.Done()
+
+	fmt.Printf("Worker %v finished and shutting down.\n", workerID)
 }
